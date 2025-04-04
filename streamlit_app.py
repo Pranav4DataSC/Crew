854e97f0f214f673e30bd608319d4999384cb69e
Pranav4DataSC <119378593+Pranav4DataSC@users.noreply.github.com>
2025-04-04 10:49:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,152 @@
 import streamlit as st
+from openai import OpenAI
+from crewai import Crew, Process, Agent, Task
+from langchain_core.callbacks import BaseCallbackHandler
+from typing import TYPE_CHECKING, Any, Dict, Optional
+from langchain_openai import ChatOpenAI
+'''
+import sqlite3
 
-st.title("🎈 My new app")
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+'''
+# Show title and description.
+st.title("💬 CrewAI Writing Studio")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "\:memo: This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
+    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
+st.write(
+    "\:robot_face: In case you do not have one and would like a demo reach out to me at https://www.linkedin.com/in/pranav-j-5a9a1388/)."
+)
+
+# Ask user for their OpenAI API key via `st.text_input`.
+# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+openai_api_key = st.text_input("OpenAI API Key", type="password")
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+else:
+
+    # Create an OpenAI client.
+    client = OpenAI(api_key=openai_api_key)
+
+    # Create a session state variable to store the chat messages. This ensures that the
+    # messages persist across reruns.
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display the existing chat messages via `st.chat_message`.
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+##  LLM Configuration
+from langchain_openai import ChatOpenAI
+llm = ChatOpenAI()
+
+avators = {"Writer":"https://cdn-icons-png.flaticon.com/512/320/320336.png",
+            "Reviewer":"https://cdn-icons-png.freepik.com/512/9408/9408201.png"}
+
+## Custom Callback Handler
+class MyCustomHandler(BaseCallbackHandler):
+    
+    def __init__(self, agent_name: str) -> None:
+        self.agent_name = agent_name
+
+    def on_chain_start(
+        self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs: Any
+    ) -> None:
+        """Print out that we are entering a chain."""
+        st.session_state.messages.append({"role": "assistant", "content": inputs['input']})
+        st.chat_message("assistant").write(inputs['input'])
+   
+    def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> None:
+        """Print out that we finished a chain."""
+        st.session_state.messages.append({"role": self.agent_name, "content": outputs['output']})
+        st.chat_message(self.agent_name, avatar=avators[self.agent_name]).write(outputs['output'])
+
+## Agent Creation
+
+writer = Agent(
+    role='Blog Post Writer',
+    backstory='''You are a blog post writer who is capable of writing a travel blog.
+                      You generate one iteration of an article once at a time.
+                      You never provide review comments.
+                      You are open to reviewer's comments and willing to iterate its article based on these comments.
+                      ''',
+    goal="Write and iterate a decent blog post.",
+    # tools=[]  # This can be optionally specified; defaults to an empty list
+    llm=llm,
+    callbacks=[MyCustomHandler("Writer")],
+    )
+reviewer = Agent(
+    role='Blog Post Reviewer',
+    backstory='''You are a professional article reviewer and very helpful for improving articles.
+                 You review articles and give change recommendations to make the article more aligned with user requests.
+                 You will give review comments upon reading entire article, so you will not generate anything when the article is not completely delivered. 
+                 You never generate blogs by itself.''',
+    goal="list builtins about what need to be improved of a specific blog post. Do not give comments on a summary or abstract of an article",
+    # tools=[]  # Optionally specify tools; defaults to an empty list
+    llm=llm,
+    callbacks=[MyCustomHandler("Reviewer")],
+)
+
+      
+    # Create a chat input field to allow the user to enter a message. This will display
+    # automatically at the bottom of the page. this was the template code hence commented.
+'''
+    if prompt := st.chat_input("What is up?"):
+
+        # Store and display the current prompt.
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # Generate a response using the OpenAI API.
+        stream = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+
+        # Stream the response to the chat using `st.write_stream`, then store it in 
+        # session state.
+        with st.chat_message("assistant"):
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
+'''
+if prompt := st.chat_input():
+
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+
+        task1 = Task(
+            description=f"""Write a blog post of {prompt}. """,
+            agent=writer,
+            expected_output="an article under 300 words."
+        )
+
+        task2 = Task(
+            description="""list review comments for improvement from the entire content of blog post to make it more viral on social media""",
+            agent=reviewer,
+            expected_output="Builtin points about where need to be improved."
+        )
+    # Establishing the crew with a hierarchical process
+        project_crew = Crew(
+            tasks=[task1, task2],  # Tasks to be delegated and executed under the manager's supervision
+            agents=[writer, reviewer],
+            manager_llm=llm,
+            process=Process.hierarchical  # Specifies the hierarchical management approach
+        )
+        final = project_crew.kickoff()
+
+        result = f"## Here is the Final Result \n\n {final}"
+        st.session_state.messages.append({"role": "assistant", "content": result})
+        st.chat_message("assistant").write(result)
+    
+    
+
